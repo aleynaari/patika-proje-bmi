9ce3afa4152eb483a26bfb7677690542aae13d19
aleyna.ari@hotmail.com <[aleyna.ari@hotmail.com]>
2021-11-29 00:28:31 +0300

    index.js

--- src/index.js
+++ src/index.js
@@ -1,34 +1,14 @@
-import { render } from "react-dom";
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
-import App from "./App";
-import Sonuclar from "./components/Sonuclar";
-import Bminedir from "./components/Bminedir";
-import Form from "./components/Form";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
-const rootElement = document.getElementById("root");
-render(
-<BrowserRouter>
- <Routes>
-     <Route path="/" element={<App />}>
-       <Route path="/bminedir" element={<Bminedir />} />
-       <Route path="/sonuclar" element={<Sonuclar />} />
-       
-           
-        
-        <Route
-         path="*"
-         element={
-           <main style={{ padding: "1rem" }}>
-             <p>There's nothing here!</p>
-           </main>
-          }
-        />
-     </Route>
-  </Routes>
-</BrowserRouter>,
-  rootElement
+ReactDOM.render(
+  <BrowserRouter>
+  <App />
+  </BrowserRouter>,
+  document.getElementById('root')
 );
+
+
