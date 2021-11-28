363424ce4828f6a8471820fc79b0deb4d7ef3793
aleyna.ari@hotmail.com <[aleyna.ari@hotmail.com]>
2021-11-29 00:26:13 +0300

    app.js

--- src/App.js
+++ src/App.js
@@ -1,63 +1,36 @@
-import { Outlet, NavLink, } from "react-router-dom";
 import './App.css';
-import styled from 'styled-components';
+import {
+  Routes,
+  Route
+} from "react-router-dom";
+import Sonuclar from "./components/Sonuclar";
+import Bminedir from "./components/Bminedir";
 import Form from "./components/Form";
+import Nav from "./components/Nav";
+//import Listele from './components/index';
 
+export default function App() {
+return (
+  <>
+  <Nav />
 
-
-const Container = styled.div`
-display: flex;
-background-color: #FFF1AF;
-height: 70px;
-width: 100%;
-`;
-
-const Header = styled.h3`
-margin-left: 30px;
-color: #66806A;
-font-weight: bolder;
-`;
-
-const Btn = styled.button`
-font-size: 15px;
-background-color: #FFF1AF;
-height: 30px;
-border: none;
-color: #66806A;
-font-weight: bolder;
-`;
-
-export default function Navbar() {
-  return (
-    <>
-       <Container>
-     <Header> BMI - VÜCUT KİTLE ENDEKSİ HESAPLAMA</Header>
-     <ul className="btnC">
-        <li>
-        <Btn className="btnS">
-          <NavLink exact to="/" >BMI Hesapla</NavLink> 
-        </Btn>
-        </li>
-        |{" "}
-        <li>
-        <Btn className="btnS">
-          <NavLink to="/bminedir">BMI Nedir</NavLink> 
-        </Btn>
-        </li>
-        |{" "}
-        <li>
-        <Btn className="btnS">
-          <NavLink to="/sonuclar">Sonuclarınız</NavLink>
-        </Btn>
-        </li>
-      </ul>
-   </Container>
-
-   <Outlet />
-   <Form></Form>   
-
-   </>
-  );
+    <Routes>
+        <Route path="/" element={<Form />} />
+          <Route path="/bminedir" element={<Bminedir />} />
+          <Route path="/sonuclar" element={<Sonuclar />} />
+            
+            <Route
+            path="*"
+            element={
+              <main style={{ padding: "1rem" }}>
+                <p>There's nothing here!</p>
+              </main>
+              }
+            />
+      </Routes>
     
-
-};
+  </>
+  
+  
+);
+}
